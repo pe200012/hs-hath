72b7bc28914fdc995c308e098b558278c3af6a5c
pe200012 <1326263755@qq.com>
2024-05-07 23:09:34 +0800

    minimal
    0.2
    
    cache refactor
    
    cache
    
    rawid conv
    
    refactor
    
    update
    
    opt out polysemy
    
    strictness
    
    inline small functions
    
    fix hset error

---
+++ app/Main.hs
@@ -0,0 +1,62 @@
+
+{-# LANGUAGE DataKinds #-}
+{-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE ScopedTypeVariables #-}
+
+module Main ( main ) where
+
+import           Colog              ( logError, logInfo, richMessageAction, usingLoggerT )
+
+import           Control.Concurrent ( forkIO, myThreadId, threadDelay, throwTo )
+import           Control.Exception  ( SomeException, catch )
+import           Control.Monad      ( forever, void, when )
+
+import           Data.IORef         ( newIORef )
+
+import qualified Dhall
+
+import           Network.HTTP.Types ( status200 )
+
+import           Prelude            hiding ( log )
+
+import           Query
+
+import           Server
+
+import           System.Exit        ( exitFailure )
+import           System.Posix       ( Handler(Catch), installHandler, sigINT, sigTERM )
+
+import           Types
+
+hathMain :: IO ()
+hathMain = do
+    config <- unmarshallClientConfig <$> Dhall.input Dhall.auto "./client-login"
+    res <- usingLoggerT richMessageAction serverStatus
+    when (res /= status200) $ do
+        usingLoggerT richMessageAction $ logError "Server status request failed"
+        exitFailure
+    clientLogin config
+    sts <- usingLoggerT richMessageAction $ Query.hathSettings config
+    hSets <- newIORef sts
+    usingLoggerT richMessageAction $ logInfo "Starting server"
+    myId <- myThreadId
+    serverId <- forkIO $ runHTTPServer hSets config
+    void $ installHandler sigINT (Catch $ do
+                                      usingLoggerT richMessageAction $ clientStop config
+                                      throwTo serverId GracefulShutdown
+                                      throwTo myId GracefulShutdown) Nothing
+    void $ installHandler sigTERM (Catch $ do
+                                       usingLoggerT richMessageAction $ clientStop config
+                                       throwTo serverId GracefulShutdown
+                                       throwTo myId GracefulShutdown) Nothing
+    usingLoggerT richMessageAction $ clientStart config
+    forever $ do
+        threadDelay (60 * periodSeconds)
+        heartBeat config `catch` (\(e :: SomeException) -> print e)
+  where
+    periodSeconds = 1000000
+
+main :: IO ()
+main = do
+    usingLoggerT richMessageAction $ logInfo "Starting Hentai@Home server"
+    hathMain
