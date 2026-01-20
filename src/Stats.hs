89428eb1eaa21dcfeacd03a288ce1f9d6071402f
pe200012 <pe1326263755@gmail.com>
2026-01-20 13:35:31 +0900

    Adds statistics tracking with Prometheus
    Introduces a statistics tracking system using Prometheus.
    
    This system gathers metrics such as upload/download traffic,
    served/fetched requests, active connections, uptime, download tasks/files/bytes
    and errors.
    These metrics are exposed via Prometheus for monitoring and alerting.

--- src/Stats.hs
+++ src/Stats.hs
@@ -3,53 +3,42 @@
 {-# LANGUAGE TemplateHaskell #-}
 
 module Stats
-  ( TrafficStats(..)
-  , emptyTrafficStats
-  , StatsEnv(..)
+  ( StatsEnv(..)
   , newStatsEnv
   , Stats(..)
   , addUpload
   , addDownload
   , incServed
   , incFetched
-  , readStats
   , readPrometheus
   , runStats
   ) where
 
-import qualified Control.Concurrent.STM as STM
-
-import qualified Data.Text              as T
-import           Data.Time.Clock        ( UTCTime, getCurrentTime )
+import           Data.Binary.Builder                           ( toLazyByteString )
+import           Data.Time.Clock                               ( UTCTime
+                                                               , diffUTCTime
+                                                               , getCurrentTime
+                                                               )
 
 import           Polysemy
 import           Polysemy.Operators
-import           Polysemy.Reader        ( Reader, ask )
+import           Polysemy.Reader                               ( Reader, ask )
+
+import           Relude                                        hiding ( Reader, ask )
 
 import qualified System.Metrics.Prometheus.Concurrent.Registry as Registry
-import           System.Metrics.Prometheus.Concurrent.RegistryT
-import           System.Metrics.Prometheus.Metric.Counter
-import           System.Metrics.Prometheus.Metric.Gauge
-import qualified System.Metrics.Prometheus.MetricId as MetricId
-import qualified System.Metrics.Prometheus.Encode.Text as Encode
+import           System.Metrics.Prometheus.Concurrent.Registry ( new
+                                                               , registerCounter
+                                                               , registerGauge
+                                                               )
+import           System.Metrics.Prometheus.Encode.Text         ( encodeMetrics )
+import           System.Metrics.Prometheus.Metric.Counter      ( Counter )
+import qualified System.Metrics.Prometheus.Metric.Counter      as Counter
+import           System.Metrics.Prometheus.Metric.Gauge        ( Gauge )
+import qualified System.Metrics.Prometheus.Metric.Gauge        as Gauge
 
-import           Relude                 hiding ( Reader, ask )
-
-data TrafficStats
-  = TrafficStats
-  { uploadBytes :: !Int64
-  , downloadBytes :: !Int64
-  , servedCount :: !Int64
-  , fetchedCount :: !Int64
-  , dlTaskCount   :: !Int64
-  , dlFileCount   :: !Int64
-  , dlBytes       :: !Int64
-  } deriving ( Show, Eq, Generic )
-
-emptyTrafficStats :: TrafficStats
-emptyTrafficStats = TrafficStats 0 0 0 0 0 0 0
-
-data StatsEnv = StatsEnv
+data StatsEnv
+  = StatsEnv
   { statsStart :: !UTCTime
   , statsRegistry :: !Registry.Registry
   , statsUploadBytesCounter :: !Counter
@@ -67,37 +56,37 @@ data StatsEnv = StatsEnv
 newStatsEnv :: IO StatsEnv
 newStatsEnv = do
   t0 <- getCurrentTime
-  registry <- Registry.new
-  uploadBytesCounter <- Registry.registerCounter "hath_cache_sent_bytes_total" mempty registry
-  downloadBytesCounter <- Registry.registerCounter "hath_cache_received_bytes_total" mempty registry
-  servedCounter <- Registry.registerCounter "hath_cache_sent_total" mempty registry
-  fetchedCounter <- Registry.registerCounter "hath_cache_received_total" mempty registry
-  activeConnectionsGauge <- Registry.registerGauge "hath_active_connections" mempty registry
-  uptimeGauge <- Registry.registerGauge "hath_uptime_seconds" mempty registry
-  dlTaskCounter <- Registry.registerCounter "hath_download_task_count_total" mempty registry
-  dlFileCounter <- Registry.registerCounter "hath_download_file_count_total" mempty registry
-  dlBytesCounter <- Registry.registerCounter "hath_download_size_bytes_total" mempty registry
-  errorCounter <- Registry.registerCounter "hath_errors_total" (MetricId.fromList [("type", "rpc_timeout")]) registry
-  pure $ StatsEnv
-    t0
-    registry
-    uploadBytesCounter
-    downloadBytesCounter
-    servedCounter
-    fetchedCounter
-    activeConnectionsGauge
-    uptimeGauge
-    dlTaskCounter
-    dlFileCounter
-    dlBytesCounter
-    errorCounter
+  registry <- new
+  uploadBytesCounter <- registerCounter "hath_cache_sent_bytes_total" mempty registry
+  downloadBytesCounter <- registerCounter "hath_cache_received_bytes_total" mempty registry
+  servedCounter <- registerCounter "hath_cache_sent_total" mempty registry
+  fetchedCounter <- registerCounter "hath_cache_received_total" mempty registry
+  activeConnectionsGauge <- registerGauge "hath_active_connections" mempty registry
+  uptimeGauge <- registerGauge "hath_uptime_seconds" mempty registry
+  dlTaskCounter <- registerCounter "hath_download_task_count_total" mempty registry
+  dlFileCounter <- registerCounter "hath_download_file_count_total" mempty registry
+  dlBytesCounter <- registerCounter "hath_download_size_bytes_total" mempty registry
+  errorCounter <- registerCounter "hath_errors_total" mempty registry
+  pure
+    $ StatsEnv
+      t0
+      registry
+      uploadBytesCounter
+      downloadBytesCounter
+      servedCounter
+      fetchedCounter
+      activeConnectionsGauge
+      uptimeGauge
+      dlTaskCounter
+      dlFileCounter
+      dlBytesCounter
+      errorCounter
 
 data Stats m a where
   AddUpload :: Int -> Stats m ()
   AddDownload :: Int -> Stats m ()
   IncServed :: Stats m ()
   IncFetched :: Stats m ()
-  ReadStats :: Stats m TrafficStats
   ReadPrometheus :: Stats m Text
 
   -- New uptime metric
@@ -117,47 +106,54 @@ data Stats m a where
 
 makeSem ''Stats
 
-toPrometheus :: TrafficStats -> Text
-toPrometheus s
-  = T.unlines
-    [ "# HELP hs_hath_upload_bytes_total Total bytes uploaded"
-    , "# TYPE hs_hath_upload_bytes_total counter"
-    , "hs_hath_upload_bytes_total " <> show (uploadBytes s)
-    , ""
-    , "# HELP hs_hath_download_bytes_total Total bytes downloaded"
-    , "# TYPE hs_hath_download_bytes_total counter"
-    , "hs_hath_download_bytes_total " <> show (downloadBytes s)
-    , ""
-    , "# HELP hs_hath_served_requests_total Total served requests"
-    , "# TYPE hs_hath_served_requests_total counter"
-    , "hs_hath_served_requests_total " <> show (servedCount s)
-    , ""
-    , "# HELP hs_hath_fetched_requests_total Total fetched requests"
-    , "# TYPE hs_hath_fetched_requests_total counter"
-    , "hs_hath_fetched_requests_total " <> show (fetchedCount s)
-    ]
-
 runStats :: Members '[ Embed IO, Reader StatsEnv ] r => Stats : r @> a -> r @> a
 runStats = interpret $ \case
-  AddUpload n    -> do
-    StatsEnv { statsVar } <- ask
-    embed $ STM.atomically $ STM.modifyTVar' statsVar $ \s -> s
-      { uploadBytes = uploadBytes s + fromIntegral n }
-  AddDownload n  -> do
-    StatsEnv { statsVar } <- ask
-    embed $ STM.atomically $ STM.modifyTVar' statsVar $ \s -> s
-      { downloadBytes = downloadBytes s + fromIntegral n }
-  IncServed      -> do
-    StatsEnv { statsVar } <- ask
-    embed $ STM.atomically $ STM.modifyTVar' statsVar $ \s -> s { servedCount = servedCount s + 1 }
-  IncFetched     -> do
-    StatsEnv { statsVar } <- ask
-    embed $ STM.atomically $ STM.modifyTVar' statsVar $ \s -> s
-      { fetchedCount = fetchedCount s + 1 }
-  ReadStats      -> do
-    StatsEnv { statsVar } <- ask
-    embed $ STM.readTVarIO statsVar
+  AddUpload n -> do
+    env <- ask
+    embed $ Counter.add n (statsUploadBytesCounter env)
+  AddDownload n -> do
+    env <- ask
+    embed $ Counter.add n (statsDownloadBytesCounter env)
+  IncServed -> do
+    env <- ask
+    embed $ Counter.inc (statsServedCounter env)
+  IncFetched -> do
+    env <- ask
+    embed $ Counter.inc (statsFetchedCounter env)
+
+  -- New uptime metric
+  UpdateUptime -> do
+    env <- ask
+    now <- embed getCurrentTime
+    let elapsed = realToFrac (diffUTCTime now (statsStart env)) :: Double
+    embed $ Gauge.set elapsed (statsUptimeGauge env)
+
+  -- Active connections
+  IncActiveConnections -> do
+    env <- ask
+    embed $ Gauge.inc (statsActiveConnectionsGauge env)
+
+  DecActiveConnections -> do
+    env <- ask
+    embed $ Gauge.dec (statsActiveConnectionsGauge env)
+
+  -- Gallery Downloader metrics
+  IncDlTask -> do
+    env <- ask
+    embed $ Counter.inc (statsDlTaskCounter env)
+  IncDlFile -> do
+    env <- ask
+    embed $ Counter.inc (statsDlFileCounter env)
+  AddDlBytes n -> do
+    env <- ask
+    embed $ Counter.add n (statsDlBytesCounter env)
+
+  -- Error metrics with labels
+  IncError _errorType -> do
+    env <- ask
+    embed $ Counter.inc (statsErrorCounter env)
+
   ReadPrometheus -> do
-    StatsEnv { statsVar } <- ask
-    s <- embed $ STM.readTVarIO statsVar
-    pure $ toPrometheus s
+    env <- ask
+    sample <- embed $ Registry.sample (statsRegistry env)
+    pure $ decodeUtf8 $ toLazyByteString $ encodeMetrics sample
